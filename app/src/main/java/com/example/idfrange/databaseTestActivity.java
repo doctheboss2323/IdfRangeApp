dc73ddd05449219a074109dbe27a22bec0c00fea
elieh <elie.hallermeier@gmail.com>
2021-07-23 00:55:57 +0300

    0.1.7
    5 activities
    
    login works
    new range works
    new drill works
    activity3
    works with rangelist(0)=" "
    score adding works
    different users adding scores works
    clean
    login only for new range works
    animation
    my scores works
    global scores works 3 scores

---
+++ app/src/main/java/com/example/idfrange/databaseTestActivity.java
@@ -0,0 +1,21 @@
+package com.example.idfrange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class databaseTestActivity extends AppCompatActivity {
+
+    private FirebaseDatabase firstDatabase= FirebaseDatabase.getInstance("https://idfrange-default-rtdb.europe-west1.firebasedatabase.app/");
+    private DatabaseReference myDatabase=firstDatabase.getReference();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_database_test);
+
+        myDatabase.setValue("Hello world");
+    }
+}
